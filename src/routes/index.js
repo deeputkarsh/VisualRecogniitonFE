d7e267e3e4f21d0151d011ed8abb3abbac0081a3
Bhagat <megha.bhagat@accenture.com>
2019-11-10 17:59:02 +0530

    Menu Item

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { Header } from '../components'
 import styles from 'Styles/index.scss'
-// import Watson from './watson'
+import Watson from './watson'
 import SpeechToText from './speechtotext'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 export default (props) => {
   return (
-    <>
+    <Router>
       <Header />
       <div className={styles.mainWrapper}>
-        {/* <Watson /> */}
-        <SpeechToText />
+        <Switch>
+          <Route exact path='/'>
+            <Watson />
+          </Route>
+          <Route exact path='/speech'>
+            <SpeechToText />
+          </Route>
+        </Switch>
       </div>
-    </>
+    </Router>
   )
 }
