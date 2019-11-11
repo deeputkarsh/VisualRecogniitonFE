c01512b763b65cc5f7096cf97e026bd79db88359
Utkarsh Deep <utkarsh_d@hdfclife.com>
2019-11-11 16:18:45 +0530

    Updates

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
     <link rel="manifest" href="/manifest.json">
     <meta name="theme-color" content="#f4f8f9"/>
     <title>Visual Recognition</title>
-  <link rel="shortcut icon" href="/favicon.ico"><link href="/main.66e7b060b51f84efc9b1.css" rel="stylesheet"></head>
+  <link rel="shortcut icon" href="/favicon.ico"><link href="/main.2e2329ad1ba4d66dfb31.css" rel="stylesheet"></head>
   <body>
     <noscript>
       You need to enable JavaScript to run this app.
     </noscript>
     <div id="root"></div>
-  <script type="text/javascript" src="/1.66e7b060b51f84efc9b1.js"></script><script type="text/javascript" src="/66e7b060b51f84efc9b1.js"></script></body>
+  <script type="text/javascript" src="/1.2e2329ad1ba4d66dfb31.js"></script><script type="text/javascript" src="/2e2329ad1ba4d66dfb31.js"></script></body>
 </html>
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c2f0e6a7f8b4c0d38026cab869052ff.js"
+  "/precache-manifest.5b8fbb6ea4125e916ab45930e310565b.js"
 );
 
 self.addEventListener('message', (event) => {
