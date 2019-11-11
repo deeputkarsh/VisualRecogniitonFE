88a0f221ea94c912b0aa38ea6161feec63ade66b
Utkarsh Deep <43804546+deeputkarsh@users.noreply.github.com>
2019-11-11 20:20:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!--
   /* Redirect http to https */
-  <?php if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  <?php if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) {
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
-    exit;
+    header('Location: ' . $redirect);
+    exit();
   } ?>-->
 <!doctype html>
 <html lang="en">
