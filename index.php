044c5f555df45919128626c23fbb59594c3ef1b4
Utkarsh Deep <43804546+deeputkarsh@users.noreply.github.com>
2019-11-11 20:07:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<!--<?php echo json_decode($_SERVER)?>-->
+<!--
+  /* Redirect http to https */
+  <?php if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+  } ?>-->
 <!doctype html>
 <html lang="en">
   <head>
