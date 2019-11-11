5fc1e7fb68a60c3e826a6a9e322e29d2efd59567
Utkarsh Deep <43804546+deeputkarsh@users.noreply.github.com>
2019-11-11 12:13:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php require 'index.html';?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <base href="/" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=yes"
+    />
+    <title>Visual Recognition</title>
+  <link rel="shortcut icon" href="/favicon.ico"></head>
+  <body>
+    <noscript>
+      You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root"></div>
+  <script type="text/javascript" src="/1.ed0f41001a5c4e9576ab.js"></script><script type="text/javascript" src="/ed0f41001a5c4e9576ab.js"></script></body>
+</html>
