bbcaf91bdbf5a6f63a4522deb8dbfcefb608dc51
Utkarsh Deep <utkarsh_d@hdfclife.com>
2019-11-11 14:24:38 +0530

    Updates

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <base href="/" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=yes"
+    />
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#f4f8f9"/>
+    <title>Visual Recognition</title>
+  <link rel="shortcut icon" href="/favicon.ico"></head>
+  <body>
+    <noscript>
+      You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root"></div>
+  <script type="text/javascript" src="/1.9c4997242139a28ffbf8.js"></script><script type="text/javascript" src="/9c4997242139a28ffbf8.js"></script></body>
+</html>
