a73679f54fe58d914aca94dd7d629557d2f4cd7a
Utkarsh Deep <utkarsh_d@hdfclife.com>
2019-11-11 11:48:42 +0530

    Updates

---
+++ precache-manifest.fc0667f8e42e6cf834b2907335bece3d.js
@@ -0,0 +1,16 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/1.ed0f41001a5c4e9576ab.js"
+  },
+  {
+    "url": "/ed0f41001a5c4e9576ab.js"
+  },
+  {
+    "revision": "bc2815e8a302b21c777807be67ee4314",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "244eb739ef451c3fc5c4da5faf27b124",
+    "url": "/index.html"
+  }
+]);
